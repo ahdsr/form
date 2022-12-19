e5f713f8f68cd04e54f17f346bb914ac5bb24ed8
Lucas <ahdsr>
2022-12-18 22:05:03 -0500

    Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import { type AppType } from 'next/dist/shared/lib/utils';
 
-import "../styles/globals.css";
+import Layout from '../components/layout';
+
+import '../styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default MyApp;
